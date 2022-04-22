f83833a889276c5799d9cd71eb8c2ed3bd5c7a80
Tayyab Ferozi <ferozitayyab@gmail.com>
2022-04-22 16:57:48 +0500

    complete

--- src/screens/Learn/index.jsx
+++ src/screens/Learn/index.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 
 import Button from "../../components/Button";
 import Section from "../../components/Section";
 import MiniLayout from "../../layouts/MiniLayout";
 
 const Learn = () => {
+  const [btnState, setBtnState] = useState();
+
+  console.log(btnState);
+
   return (
     <MiniLayout>
-      <Section id="learn" className="pt-0">
+      <Section
+        id="learn"
+        className={clsx(
+          btnState === "crypto" && "crypto",
+          btnState === "nft" && "nft",
+          "pt-0"
+        )}
+      >
         <div className="learn-main">
           <div className="d-flex justify-content-between align-items-center">
-            <Button gradient>NFT</Button>
-            <Button gradient>Crypto</Button>
+            <Button onClick={() => setBtnState("nft")} gradient>
+              NFT
+            </Button>
+            <Button onClick={() => setBtnState("crypto")} gradient>
+              Crypto
+            </Button>
           </div>
 
           <p>
